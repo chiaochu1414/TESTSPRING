b28c6c3f98d17e01226d72011d03b9bbb52557e0
chiao.chu <chiaochu1014@gmail.com>
2023-09-24 08:57:21 +0800

    update 0924

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -5,12 +5,13 @@ public class User {
     public String name;
     public Integer age;
 
-
+    public User() {
+        // 无参构造函数
+    }
     public  User(String name,Integer age){
         this.name=name;
         this.age=age;
+
     }
-
-
 
 }
